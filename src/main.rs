1ef24c01997d418424111fd8ac47dcee4f923beb
myyrakle <sssang97@naver.com>
2024-10-24 00:22:29 +0900

    [#1] get_app_path 구현

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub(crate) mod action;
 pub(crate) mod command;
+pub mod config;
 pub mod platform_specific;
 
 fn main() {
--- src/platform_specific.rs
+++ src/platform_specific.rs
@@ -1,19 +1,16 @@
 use std::path::PathBuf;
 
-pub fn get_config_path() -> PathBuf {
+pub fn get_app_path() -> PathBuf {
     let home_dir = std::env::var("HOME").unwrap_or_else(|_| String::from(""));
 
     #[cfg(target_os = "windows")]
-    let app_data_path = PathBuf::from(format!(r"{}\AppData\Local\magiceye\config.json", home_dir));
+    let app_data_path = PathBuf::from(format!(r"{}\AppData\Local\magiceye", home_dir));
 
     #[cfg(target_os = "macos")]
-    let app_data_path = PathBuf::from(format!(
-        "{}/Library/Application Support/magiceye.json",
-        home_dir
-    ));
+    let app_data_path = PathBuf::from(format!("{}/Library/Application Support", home_dir));
 
     #[cfg(target_os = "linux")]
-    let app_data_path = PathBuf::from(format!("{}/.local/share/magiceye.json", home_dir));
+    let app_data_path = PathBuf::from(format!("{}/.local/share", home_dir));
 
     // Ensure the directory exists
     if !app_data_path.exists() {
