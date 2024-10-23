ddf3d23ff3e815ffbef79aee13470ab2cf062658
myyrakle <sssang97@naver.com>
2024-10-24 00:38:00 +0900

    [#6] database pair 설정값 추가

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,27 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Config {}
+pub enum DatabaseType {
+    Postgres,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DatabasePair {
+    pub name: String,
+    pub database_type: DatabaseType,
+    pub base_connection: String,
+    pub target_connection: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub database_pairs: Vec<DatabasePair>,
+}
 
 impl Default for Config {
     fn default() -> Self {
-        Self {}
+        Self {
+            database_pairs: vec![],
+        }
     }
 }
