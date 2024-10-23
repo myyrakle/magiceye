06598703bdb5cc66415737b349c8754f65769824
myyrakle <sssang97@naver.com>
2024-10-24 00:39:06 +0900

    [#6] language 설정값 추가

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,18 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub enum Language {
+    English,
+    Korean,
+}
+
+impl Default for Language {
+    fn default() -> Self {
+        Self::English
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub enum DatabaseType {
     Postgres,
 }
@@ -16,12 +28,14 @@ pub struct DatabasePair {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub database_pairs: Vec<DatabasePair>,
+    pub current_language: Language,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             database_pairs: vec![],
+            current_language: Language::default(),
         }
     }
 }
